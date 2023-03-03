3482934ce1e08980f3c61218a308436ac493fe51
ssah <Shubham_Sah@intuit.com>
2023-03-03 09:50:00 -0800

    fix structure

--- superman/selectionsort.py
+++
@@ -1,8 +0,0 @@
-def selection_sort(my_list):
-    for i in range(len(my_list)-1):
-        min_index=i
-        for j in range(i+1,len(my_list)):
-            if my_list[j] < my_list[min_index]:
-                min_index=j
-            temp = my_list[i]
-            
