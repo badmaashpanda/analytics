79d71f52ef0dbf31ab0fdbea87a1508a2158c677
ssah <Shubham_Sah@intuit.com>
2023-03-04 18:00:47 -0800

    ss1

---
+++ stratascratch/sql/100/10045.sql
@@ -0,0 +1,5 @@
+-- Points Rating Of Wines Over Time
+
+-- Find the average points difference between each and previous years starting from the year 2000. Output the year, average points, previous average points, and the difference between them.
+-- If you're unable to calculate the average points rating for a specific year, use an 87 average points rating for that year (which is the average of all wines starting from 2000).
+
